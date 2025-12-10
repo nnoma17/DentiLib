63dcdb6c54987624ed645191a5e8d4e1f2aad585
Manon <vatresmanon17@gmail.com>
2025-12-10 15:31:02 +0100

    Project init

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Bienvenido a mi servidor Express, estoy funcionando correctamente, mi amogo! Vamos a codear juntos!');
+
+    
+})
