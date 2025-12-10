5c81273f43e143c5d0844bba7ecc13a56285446a
Manon <vatresmanon17@gmail.com>
2025-12-10 16:30:30 +0100

    connection mongoose

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
 
+//Connection database
+const dbConnection = require('./config/dbConfig');
+
+dbConnection();
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
