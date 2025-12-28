95439411ab50ded554a84c9179835ad23e8ee218
Manon <vatresmanon17@gmail.com>
2025-12-28 12:27:43 +0100

    mongo_uri

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDatabase = async () => {
     try {
-        await mongoose.connect(process.env.DB_URL);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MONGO_URI =", process.env.MONGO_URI?.slice(0, 20));
         console.log('Database connected successfully');
     } catch (error) {
