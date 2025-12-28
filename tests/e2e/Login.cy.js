614a464544316f240a233846594f61734f3c4b3c
Manon <vatresmanon17@gmail.com>
2025-12-28 12:34:50 +0100

    pipeline

--- tests/e2e/Login.cy.js
+++ tests/e2e/Login.cy.js
@@ -4,28 +4,6 @@ describe('page acceuil', () => {
   beforeEach( () => {
     cy.visit("http://localhost:3000");
   });
-  
-  it('Connexion sans email', () => {
-    cy.get('#password').type('mauvaisMDP');
-    cy.xpath('//button[text()="Se connecter"]').click();
-    //une erreur s'affiche
-    cy.get('#errorLogin').should('be.visible').and('contain', "Veuillez remplir tous les champs");
-  });
-
-  it("Test envoi d'une requete sans mail",() => {
-    cy.intercept('POST', 'http://localhost:3000/api/user/login_User', {
-        statusCode: 400,
-        body: {
-          success: false,
-          message: "Champs manquants"
-      }
-    }).as('loginRequest');
-
-    cy.get('#password').type('mauvaisMDP');
-    cy.xpath('//button[text()="Se connecter"]').click();
-    cy.get('#errorLogin').should('be.visible').and('contain', "Veuillez remplir tous les champs");
-
-  });
 
   it('Connexion OK', () => {
     cy.get('#email').type('email@gmail.com');
@@ -57,5 +35,27 @@ describe('page acceuil', () => {
     cy.xpath('//button[text()="Se connecter"]').click();
     //une erreur s'affiche
     cy.get('#errorLogin').should('be.visible').and('contain', "L'utilisateur n'existe pas");
+  });
+  
+  it('Connexion sans email', () => {
+    cy.get('#password').type('mauvaisMDP');
+    cy.xpath('//button[text()="Se connecter"]').click();
+    //une erreur s'affiche
+    cy.get('#errorLogin').should('be.visible').and('contain', "Veuillez remplir tous les champs");
+  });
+
+  it("Test envoi d'une requete sans mail",() => {
+    cy.intercept('POST', 'http://localhost:3000/api/user/login_User', {
+        statusCode: 400,
+        body: {
+          success: false,
+          message: "Champs manquants"
+      }
+    }).as('loginRequest');
+
+    cy.get('#password').type('mauvaisMDP');
+    cy.xpath('//button[text()="Se connecter"]').click();
+    cy.get('#errorLogin').should('be.visible').and('contain', "Veuillez remplir tous les champs");
+
   });
 });
