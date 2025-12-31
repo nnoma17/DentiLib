cc3e539ad4a9c61678541263ad5fdecc5e4e13cb
Manon <vatresmanon17@gmail.com>
2025-12-31 11:31:20 +0100

    close test

--- tests/integration/login.test.js
+++ tests/integration/login.test.js
@@ -1,13 +1,18 @@
 const request = require("supertest");
 const app = require("../../app");
 const dbConnection = require("../../config/dbConfig");
+const mongoose = require("mongoose");
 
 beforeAll(async () => {
     await dbConnection();
 });
 
-describe("POST /api/user/login_User", () => {
+afterAll(async () => {
+    await mongoose.connection.close(); // <-- ferme la connexion MongoDB
+});
 
+describe("POST /api/user/login_User", () => {
+    jest.setTimeout(20000);
     test("login valide", async () => {
         const response = await request(app)
             .post("/api/user/login_User")
