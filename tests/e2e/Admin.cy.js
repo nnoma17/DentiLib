ad01782fe3f40691473cc3c296565f4b6a5b53dd
Manon <vatresmanon17@gmail.com>
2025-12-31 11:56:34 +0100

    cypress correct

--- tests/e2e/Admin.cy.js
+++ tests/e2e/Admin.cy.js
@@ -2,7 +2,7 @@ require('cypress-xpath');
 
 describe('page acceuil', () => {
   beforeEach( () => {
-    cy.visit("https://dentilib-5sk3.onrender.com/");
+    cy.visit("http://localhost:3000");
 
     cy.get('#email').type('email@gmail.com');
     cy.get('#password').type('12345678');
