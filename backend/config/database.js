b2d099d4f38916d835b47505c63e4543f4b83458
Manon <vatresmanon17@gmail.com>
2026-06-12 08:53:23 +0200

    base de donne clvercloud

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  'dentilib',
-  'dentilib_user',
-  'admin',
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: 'localhost',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     logging: false
   }
